78f6ffc2693ef734946647cd03aa3753ac912407
sh20raj <shaswatraj3@gmail.com>
2024-05-07 19:02:23 +0530

    Conference Proceedings

--- src/app/conference-paper/page.js
+++ src/app/conference-paper/page.js
@@ -2,77 +2,78 @@
 export default function page() {
   return (
     <>
-   <div className="gdlr-core-accordion-item-content-wrapper">
-  <h4 className="gdlr-core-accordion-item-title gdlr-core-js  gdlr-core-skin-e-background gdlr-core-skin-e-content" style={{fontSize: 20, textTransform: 'none'}}>
-    <i className="fa fa-book" /> Conference Proceedings</h4>
-  <div className="gdlr-core-accordion-item-content">
-    <div className="alert alert-success"><p>  <a href="#2022" className="btn btn-default">2022</a>
-        <a href="#2020" className="btn btn-default">2020</a>
-        <a href="#2019" className="btn btn-default">2019</a>
-        <a href="#2018" className="btn btn-default">2018</a>
-        <a href="#2017" className="btn btn-default">2017</a>
-        <a href="#2016" className="btn btn-default">2016</a>
-        <a href="#2015" className="btn btn-default">2015</a>
-        <a href="#2014" className="btn btn-default">2014</a>
-        <a href="#2013" className="btn btn-default">2013</a>
-      </p></div>    
-      <div className="blog">
-    <a name={2022} id={2022} />
-    <h4 style={{fontSize: 20, textTransform: 'none'}}><u>2022</u></h4>            
-    <p style={{fontSize: 17}}>1. <b>Sujit Biswas, Rajat S Goswami</b> <font color="#006699">"Understanding Quantum Computing Through Drunken Walks"</font>,  Pattern Recognition and Data Analysis with Applications , 2022, vol. , pp. 635-646&nbsp;<a className=" text-danger" href="https://link.springer.com/chapter/10.1007/978-981-19-1520-8_52">(https://link.springer.com/chapter/10.1007/978-981-19-1520-8_52)</a>
-    </p><p style={{fontSize: 17}}>2. <b>K Hemant K Reddy, Rajat S Goswami, Shubham</b> <font color="#006699">"An Energy-Aware Fog-Enabled Optimized VM Consolidation Scheme for Real-Time Applications"</font>, Pattern Recognition and Data Analysis with Applications, 2022
-    </p> 
-    <a name={2020} id={2020} />
-    <h4 style={{fontSize: 20, textTransform: 'none'}}><u>2020</u></h4>            
-    <p style={{fontSize: 17}}>1. <b>Chukhu Chunka, Subhasish Banerjee, Soumyajit Nag, Rajat Subhra Goswami</b> <font color="#006699">"A Secure Key Agreement Protocol for Data Communication in Public Network Based on the Diffie-Hellman Key Agreement Protocol"</font>, Micro-Electronics and Telecommunication Engineering: Proceedings of 3rd ICMETE 2019, 2020, pp. 531-543
-    </p> 
-    <a name={2019} id={2019} />
-    <h4 style={{fontSize: 20, textTransform: 'none'}}><u>2019</u></h4>            
-    <p style={{fontSize: 17}}>1. <b>Chukhu Chunka, Subhasish Banerjee, Rajat Subhra Goswami</b> <font color="#006699">"A Novel Key Generating Scheme of Automatic Variable Key
-        "</font>, 2019 IEEE International Conference on Advanced Networks and Telecommunications Systems (ANTS), 2019, pp. 1-5&nbsp;<a className=" text-danger" href="https://ieeexplore.ieee.org/abstract/document/9118041">(https://ieeexplore.ieee.org/abstract/document/9118041)</a>
-    </p><p style={{fontSize: 17}}>2. <b>Chukhu Chunka, Rajat Subhra Goswami, Subhasish Banerjee</b> <font color="#006699">"A novel approach to generate symmetric key in cryptography using genetic algorithm (ga)"</font>, Emerging Technologies in Data Mining and Information Security: Proceedings of IEMIS 2018, Volume 1, 2019
-    </p><p style={{fontSize: 17}}>3. <b>ilapati Waikhom, Rajat Subhra Goswami</b> <font color="#006699">"Proceedings of International Conference on Advancements in Computing &amp; Management (ICACM)"</font>, International Conference on Advancements in Computing &amp; Management (ICACM-2019), 2019
-    </p> 
-    <a name={2018} id={2018} />
-    <h4 style={{fontSize: 20, textTransform: 'none'}}><u>2018</u></h4>            
-    <p style={{fontSize: 17}}>1. <b>Moumita Das, Rajat Subhra Goswami, Manash P Dutta, SK Chakraborty, CT Bhunia</b> <font color="#006699">"Generating Variable Keys in Automatic Variable Key with Noise Burst Bit in Cryptography towards Data Transfer for Achieving Perfect Security"</font>, 2018 3rd International Conference for Convergence in Technology (I2CT), 2018, pp. 1-6&nbsp;<a className=" text-danger" href="https://ieeexplore.ieee.org/abstract/document/8529384">(https://ieeexplore.ieee.org/abstract/document/8529384)</a>
-    </p> 
-    <a name={2017} id={2017} />
-    <h4 style={{fontSize: 20, textTransform: 'none'}}><u>2017</u></h4>            
-    <p style={{fontSize: 17}}>1. <b>Moumita Das, Rajat Subhra Goswami, Manash P Dutta, SK Chakraborty, CT Bhunia</b> <font color="#006699">"Methods to generate variable keys with noise burst bit in modern cryptosystem for achieving perfect security"</font>, 2017 Fourth International Conference on Image Information Processing (ICIIP), 2017, pp. 1-6&nbsp;<a className=" text-danger" href="https://ieeexplore.ieee.org/abstract/document/8313695">(https://ieeexplore.ieee.org/abstract/document/8313695)</a>
-    </p><p style={{fontSize: 17}}>2. <b>Subhendu Sekhar Paik, Rajat Subhra Goswami, DS Roy, K Hemant Reddy</b> <font color="#006699">"Intelligent Data Placement in Heterogeneous Hadoop Cluster"</font>, International Conference on Next Generation Computing Technologies, 2017, pp. 568-579&nbsp;<a className=" text-danger" href="https://link.springer.com/chapter/10.1007/978-981-10-8657-1_43">(https://link.springer.com/chapter/10.1007/978-981-10-8657-1_43)</a>
-    </p> 
-    <a name={2016} id={2016} />
-    <h4 style={{fontSize: 20, textTransform: 'none'}}><u>2016</u></h4>            
-    <p style={{fontSize: 17}}>1. <b>Nani Yalu, RajatSubhra Goswami, Subhasish Banerjee</b> <font color="#006699">"An efficient packet hiding method for preventing jamming attacks in wireless networks"</font>, 2016 International Conference on Wireless Communications, Signal Processing and Networking (WiSPNET), 2016, pp. 2318-2321&nbsp;<a className=" text-danger" href="https://ieeexplore.ieee.org/abstract/document/7566556">(https://ieeexplore.ieee.org/abstract/document/7566556)</a>
-    </p> 
-    <a name={2015} id={2015} />
-    <h4 style={{fontSize: 20, textTransform: 'none'}}><u>2015</u></h4>            
-    <p style={{fontSize: 17}}>1. <b>Rajat Subhra Goswami, Subhasish Banerjee, Manash Pratim Dutta, Chandan T Bhunia</b> <font color="#006699">"Absolute key variation technique of automatic variable key in cryptography"</font>, Proceedings of the 8th International Conference on Security of Information and Networks, 2015, pp. 65-67&nbsp;<a className=" text-danger" href="https://dl.acm.org/doi/abs/10.1145/2799979.2800021">(https://dl.acm.org/doi/abs/10.1145/2799979.2800021)</a>
-    </p> 
-    <a name={2014} id={2014} />
-    <h4 style={{fontSize: 20, textTransform: 'none'}}><u>2014</u></h4>            
-    <p style={{fontSize: 17}}>1. <b>Swarnendu K Chakraborty, Rajat Subhra Goswami, Abhinandan Bhunia, Chandan Tilak Bhunia</b> <font color="#006699">"Method of Non Majority Decision Making in Aggressive Packet Combining Scheme
-        "</font>, 2014 11th International Conference on Information Technology: New Generations, 2014, pp. 608-609&nbsp;<a className=" text-danger" href="https://ieeexplore.ieee.org/abstract/document/6822265">(https://ieeexplore.ieee.org/abstract/document/6822265)</a>
-    </p><p style={{fontSize: 17}}>2. <b>Swarnendu K Chakraborty, Rajat S Goswami, Abhinandan Bhunia, Chandan T Bhunia</b> <font color="#006699">"New protocol of aggressive packet combining in Gilbert two state model
-        "</font>, Proceedings of The 2014 International Conference on Control, Instrumentation, Energy and Communication (CIEC), 2014, pp. 683-685&nbsp;<a className=" text-danger" href="https://ieeexplore.ieee.org/abstract/document/6959177">(https://ieeexplore.ieee.org/abstract/document/6959177)</a>
-    </p> 
-    <a name={2013} id={2013} />
-    <h4 style={{fontSize: 20, textTransform: 'none'}}><u>2013</u></h4>            
-    <p style={{fontSize: 17}}>1. <b>Swarnendu K Chakraborty, Rajat S Goswami, Abhinandan Bhunia, Chandan T Bhunia</b> <font color="#006699">"Two New Modified Schemes of Aggressive Packet Combining Schemes in Achieving Better Throughput
-        "</font>, 2013 10th International Conference on Information Technology: New Generations, 2013, pp. 729-730&nbsp;<a className=" text-danger" href="https://ieeexplore.ieee.org/abstract/document/6614395">(https://ieeexplore.ieee.org/abstract/document/6614395)</a>
-    </p><p style={{fontSize: 17}}>2. <b>Rajat Subhra Goswami, Swarnendu Kumar Chakraborty, Abhinandan Bhunia, Chandan Tilak Bhunia</b> <font color="#006699">"New approach towards generation of Automatic Variable Key to achieve Perfect Security
-        "</font>, 2013 10th International Conference on information technology: new generations, 2013, pp. 489-491&nbsp;<a className=" text-danger" href="https://ieeexplore.ieee.org/abstract/document/6614354">(https://ieeexplore.ieee.org/abstract/document/6614354)</a>
-    </p><p style={{fontSize: 17}}>3. <b>Rajat Subhra Goswami, Swarnendu Kumar Chakraborty, Abhinandan Bhunia, CT Bhunia</b> <font color="#006699">"Various new methods of implementing AVK
-        "</font>, Proceedings of the 2nd International Conference Advanced Computer Science and Engineering, 2013, pp. 149-152&nbsp;<a className=" text-danger" href="https://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.861.6664&rep=rep1&type=pdf">(https://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.861.6664&amp;rep=rep1&amp;type=pdf)</a>
-    </p><p style={{fontSize: 17}}>4. <b>Swarnendu K Chakraborty, Rajat S Goswami, Abhinandan Bhunia, Chandan T Bhunia</b> <font color="#006699">"Aggressive Packet Combining Schemes
-        "</font>, National Conference on Emerging Trends In Computing-NCETIC, 2013
-    </p><p style={{fontSize: 17}}>5. <b>Rajat Subhra Goswami, Swarnendu Kumar Chakraborty, Abhinandan Bhinia3 Chandan T Bhunia</b> <font color="#006699">"Review and Suggestions of Time Variant Key (Automatic Variable Key) Techniques
-        "</font>, National Conference on Emerging Trends In Computing-NCETIC, 2013
-    </p> 
+<div className="gdlr-core-accordion-item-content">
+  <div className="alert alert-success"><p>  <a href="#2023" className="btn btn-default">2023</a>
+      <a href="#2022" className="btn btn-default">2022</a>
+      <a href="#2021" className="btn btn-default">2021</a>
+      <a href="#2019" className="btn btn-default">2019</a>
+      <a href="#2018" className="btn btn-default">2018</a>
+      <a href="#2017" className="btn btn-default">2017</a>
+      <a href="#2015" className="btn btn-default">2015</a>
+      <a href="#2014" className="btn btn-default">2014</a>
+      <a href="#2013" className="btn btn-default">2013</a>
+      <a href="#2004" className="btn btn-default">2004</a>
+    </p></div>    
+    <div className="blog">
+
+
+  <a name={2023} id={2023} />
+  <h4 style={{fontSize: 20, textTransform: 'none'}}><u>2023</u></h4>            
+  <p style={{fontSize: 17}}>1. <b>Ashish Singh Parihar, Swarnendu Kr. Chakraborty</b> <font color="#006699">"Flying Ad Hoc Network (FANET): Opportunities, Trending Applications and Simulators"</font>, IEEE Pune Section International Conference (PuneCon), Pune, India, 2023, pp. 1-5&nbsp;<a className=" text-danger" href="https://doi.org/10.1109/PuneCon55413.2022.10014779">(https://doi.org/10.1109/PuneCon55413.2022.10014779)</a>
+  </p><p style={{fontSize: 17}}>2. <b>R Jerang, S Nayak, GK Mahato, SK Chakraborty</b> <font color="#006699">"Privacy-Preserving of Edge Intelligence using Homomorphic Encryption"</font>, 2023 3rd International Conference on Intelligent Technologies (CONIT), 1-6, 2023
+  </p><p style={{fontSize: 17}}>3. <b>J Dasari, TS Joshith, DD Lokesh, SS Kumar, GK Mahato, SK Chakraborty</b> <font color="#006699">"Privacy-Preserving sensitive data on Medical diagnosis using Federated Learning and Homomorphic Re-encryption"</font>, 2023 3rd International Conference on Intelligent Technologies (CONIT), 1-7, 2023
+  </p><p style={{fontSize: 17}}>4. <b>SS Kumar, TS Joshith, DD Lokesh, D Jahnavi, GK Mahato, Sk Chakraborty</b> <font color="#006699">"Privacy-Preserving and Verifiable Decentralized Federated Learning"</font>, 2023 5th International Conference on Energy, Power and Environment, 2023
+  </p> 
+  <a name={2022} id={2022} />
+  <h4 style={{fontSize: 20, textTransform: 'none'}}><u>2022</u></h4>            
+  <p style={{fontSize: 17}}>1. <b>M Kundu, NV Teja, SK Chakraborty</b> <font color="#006699">"Investigation of New Protocol: Cyclic Shift in Aggressive Packet Combining Scheme to Combat the Error in Same Bit Location"</font>, 2022 Second International Conference on Advances in Electrical, Computing …	, 2022
+  </p><p style={{fontSize: 17}}>2. <b>M Kundu, SK Chakraborty, A Sarkar, DJ Nagendra Kumar</b> <font color="#006699">"Aggressive Packet Combining Scheme for Co-Operative Wireless Communication Ensuring Enhanced Throughput"</font>, International Conference on Artificial Intelligence and Sustainable …	, 2022
+  </p><p style={{fontSize: 17}}>3. <b>Ashish Singh Parihar, Swarnendu Kr. Chakraborty</b> <font color="#006699">"A Cross-Sectional Study on Distributed Mutual Exclusion Algorithms for Ad Hoc Networks"</font>, Pattern Recognition and Data Analysis with Applications. Lecture Notes in Electrical Engineering, vol 888. Springer, Singapore., 2022, vol.888&nbsp;<a className=" text-danger" href="https://link.springer.com/chapter/10.1007/978-981-19-1520-8_3">(https://link.springer.com/chapter/10.1007/978-981-19-1520-8_3)</a>
+  </p> 
+  <a name={2021} id={2021} />
+  <h4 style={{fontSize: 20, textTransform: 'none'}}><u>2021</u></h4>            
+  <p style={{fontSize: 17}}>1. <b>Aritra Dutta, Rajesh Bose, Swarnendu Chakraborty, Haraprasad Mondal, S Roy</b> <font color="#006699">"Data Security Mechanism for Green Cloud"</font>, International Conference on Innovations in Energy Management and Renewable Resources (IEMRE 2021)At: Institute of Engineering &amp; Management, Kolkata, 2021
+  </p> 
+  <a name={2019} id={2019} />
+  <h4 style={{fontSize: 20, textTransform: 'none'}}><u>2019</u></h4>            
+  <p style={{fontSize: 17}}>1. <b>P Bardalai, N Medhi, SK Chakraborty</b> <font color="#006699">"DoubleTrApp: A Weak Vertex Cover based DDoS Detection and Mitigation scheme using SDN approach"</font>, 2019 IEEE International Conference on Advanced Networks and …	, 2019
+  </p><p style={{fontSize: 17}}>2. <b>M Kundu, SK Chakraborty, A Sarkar</b> <font color="#006699">"Modifications on Aggressive Packet Combining Scheme: An Extension to ARQ Techniques"</font>, Proceedings of International Conference on Sustainable Computing in Science …	, 2019
+  </p> 
+  <a name={2018} id={2018} />
+  <h4 style={{fontSize: 20, textTransform: 'none'}}><u>2018</u></h4>            
+  <p style={{fontSize: 17}}>1. <b>M Maiti, SK Saw, V Nath, SK Chakraborty, A Majumder</b> <font color="#006699">"A 46.8 µW/1.12 GHz 7th Stage New Ring Voltage Controlled Oscillator"</font>, International Conference on Nanoelectronics, Circuits and Communication …	, 2018
+  </p><p style={{fontSize: 17}}>2. <b>T Rahman, SK Chakraborty</b> <font color="#006699">"Provisioning technical interoperability within Zigbee and BLE in IoT environment"</font>, 2018 2nd International Conference on Electronics, Materials Engineering …	, 2018
+  </p><p style={{fontSize: 17}}>3. <b>M Das, RS Goswami, MP Dutta, SK Chakraborty, CT Bhunia</b> <font color="#006699">"Generating Variable Keys in Automatic Variable Key with Noise Burst Bit in Cryptography towards Data Transfer for Achieving Perfect Security"</font>, 2018 3rd International Conference for Convergence in Technology (I2CT), 1-6	, 2018
+  </p> 
+  <a name={2017} id={2017} />
+  <h4 style={{fontSize: 20, textTransform: 'none'}}><u>2017</u></h4>            
+  <p style={{fontSize: 17}}>1. <b>M Das, RS Goswami, MP Dutta, SK Chakraborty, CT Bhunia</b> <font color="#006699">"Methods to generate variable keys with noise burst bit in modern cryptosystem for achieving perfect security"</font>, 2017 Fourth International Conference on Image Information Processing (ICIIP …	, 2017
+  </p><p style={{fontSize: 17}}>2. <b>SK Saw, P Meher, SK Chakraborty</b> <font color="#006699">"Design of high frequency D flip flop circuit for phase detector application"</font>, TENCON 2017-2017 IEEE Region 10 Conference, 229-233	, 2017
+  </p><p style={{fontSize: 17}}>3. <b>A Sarkar, S Maity, P Chakraborty, SK Chakraborty</b> <font color="#006699">"Factors Influencing On Sensitivity of the Metal Oxide Gas Sensors"</font>, International Conference on Sustainable and Renewable Energy Development and …	, 2017
+  </p> 
+  <a name={2015} id={2015} />
+  <h4 style={{fontSize: 20, textTransform: 'none'}}><u>2015</u></h4>            
+  <p style={{fontSize: 17}}>1. <b>S Ningthoujam, SK Chakraborty, CT Bhunia, K Sarma </b> <font color="#006699">"Technical Session-I: Network Protocols, Venue: Room# 2"</font>, International Conference on Computing and Communication Systems (I3CS’15) 9, 10	, 2015
+  </p><p style={{fontSize: 17}}>2. <b>S Ningthoujam, SK Chakraborty, CT Bhunia</b> <font color="#006699">"New protocol for aggressive packet combining in gilbert two state model using back up routes to achieve higher throughput"</font>, Proceedings of the 2015 International Conference on Advanced Research in …	, 2015
+  </p> 
+  <a name={2014} id={2014} />
+  <h4 style={{fontSize: 20, textTransform: 'none'}}><u>2014</u></h4>            
+  <p style={{fontSize: 17}}>1. <b>SK Chakraborty, RS Goswami, A Bhunia, CT Bhunia</b> <font color="#006699">"Method of Non Majority Decision Making in Aggressive Packet Combining Scheme"</font>, 2014 11th International Conference on Information Technology: New …	, 2014
+  </p><p style={{fontSize: 17}}>2. <b>SK Chakraborty, RS Goswami, A Bhunia, CT Bhunia</b> <font color="#006699">"New protocol of aggressive packet combining in Gilbert two state model"</font>, Proceedings of The 2014 International Conference on Control, Instrumentation …	, 2014
+  </p> 
+  <a name={2013} id={2013} />
+  <h4 style={{fontSize: 20, textTransform: 'none'}}><u>2013</u></h4>            
+  <p style={{fontSize: 17}}>1. <b>SK Chakraborty, RS Goswami, A Bhunia, CT Bhunia</b> <font color="#006699">"Two New Modified Schemes of Aggressive Packet Combining Schemes in Achieving Better Throughput"</font>, 2013 10th International Conference on Information Technology: New …	, 2013
+  </p><p style={{fontSize: 17}}>2. <b>RS Goswami, SK Chakraborty, A Bhunia, CT Bhunia</b> <font color="#006699">"New approach towards generation of Automatic Variable Key to achieve Perfect Security"</font>, 2013 10th International Conference on information technology: new …	 , 2013
+  </p><p style={{fontSize: 17}}>3. <b>RS Goswami, SK Chakraborty, A Bhunia, CT Bhunia</b> <font color="#006699">"Various new methods of implementing AVK"</font>, Proceedings of the 2nd International Conference Advanced Computer Science …	, 2013
+  </p> 
+  <a name={2004} id={2004} />
+  <h4 style={{fontSize: 20, textTransform: 'none'}}><u>2004</u></h4>            
+  <p style={{fontSize: 17}}>1. <b>SK Chakraborty, RS Goswami, A Bhunia, CT Bhunia</b> <font color="#006699">"Aggressive Packet Combining Schemes"</font>, National Conference on Emerging Trends In Computing-NCETIC 13, 16	, 2004
+  </p> 
     </div>
-  </div>
 </div>
+
 
     </>
   )
